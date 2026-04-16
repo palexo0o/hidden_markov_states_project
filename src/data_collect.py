9ddf498dc52fe7d946d9ee6629b64fef303d2d60
palexo0o <alexopoulosphilip6@gmail.com>
2026-04-16 16:10:27 +0200

    coded data_collect, loaded data into pd.dataframes using open-meteo API via get request

---
+++ src/data_collect.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas as pd
+import hmmlearn as hm
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://archive-api.open-meteo.com/v1/archive"
+params = {
+	"latitude": 40.41,
+	"longitude": -3.7,
+	"start_date": "2026-03-10",
+	"end_date": "2026-03-20",
+	"hourly": ["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature", "precipitation", "surface_pressure", "cloud_cover", "cloud_cover_mid", "wind_speed_10m", "wind_speed_100m", "wind_direction_10m", "wind_direction_100m", "direct_radiation"],
+}
+responses = openmeteo.weather_api(url, params = params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation: {response.Elevation()} m asl")
+print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+hourly_dew_point_2m = hourly.Variables(2).ValuesAsNumpy()
+hourly_apparent_temperature = hourly.Variables(3).ValuesAsNumpy()
+hourly_precipitation = hourly.Variables(4).ValuesAsNumpy()
+hourly_surface_pressure = hourly.Variables(5).ValuesAsNumpy()
+hourly_cloud_cover = hourly.Variables(6).ValuesAsNumpy()
+hourly_cloud_cover_mid = hourly.Variables(7).ValuesAsNumpy()
+hourly_wind_speed_10m = hourly.Variables(8).ValuesAsNumpy()
+hourly_wind_speed_100m = hourly.Variables(9).ValuesAsNumpy()
+hourly_wind_direction_10m = hourly.Variables(10).ValuesAsNumpy()
+hourly_wind_direction_100m = hourly.Variables(11).ValuesAsNumpy()
+hourly_direct_radiation = hourly.Variables(12).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end =  pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+hourly_data["dew_point_2m"] = hourly_dew_point_2m
+hourly_data["apparent_temperature"] = hourly_apparent_temperature
+hourly_data["precipitation"] = hourly_precipitation
+hourly_data["surface_pressure"] = hourly_surface_pressure
+hourly_data["cloud_cover"] = hourly_cloud_cover
+hourly_data["cloud_cover_mid"] = hourly_cloud_cover_mid
+hourly_data["wind_speed_10m"] = hourly_wind_speed_10m
+hourly_data["wind_speed_100m"] = hourly_wind_speed_100m
+hourly_data["wind_direction_10m"] = hourly_wind_direction_10m
+hourly_data["wind_direction_100m"] = hourly_wind_direction_100m
+hourly_data["direct_radiation"] = hourly_direct_radiation
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+print("\nHourly data\n", hourly_dataframe)
+
+# preparing data for hnm
+
+# normalizing values
